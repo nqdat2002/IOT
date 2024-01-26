3c29c149fb9a524faf9a8ce063d1d0c9a39439e4
nqdat2002 <datfeed.2706@gmail.com>
2024-01-26 16:53:12 +0700

    Create Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {NavBar} from "./components/NavBar";
+import {Routes, Route} from "react-router-dom";
+import {Dashboard, DataSensors, History, Profile} from "./components/pages/index";
+// import {DataSensors} from "./components/pages/DataSensors";
+// import {History} from "./components/pages/History";
+// import {Profile} from "./components/pages/Profile";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={Dashboard}/>
+        <Route path="/datasensor" element={DataSensors}/>
+        <Route path="/history" element={History}/>
+        <Route path="/profile" element={Profile}/>
+      </Routes>
     </div>
   );
 }
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {NavLink } from 'react-router-dom';
+import "./Navbar.css";
+export const NavBar = () => {
+	return (
+		<header>
+			<ul>
+				<li>
+					<NavLink to="/">Dashboard</NavLink>
+				</li>
+				<li>
+					<NavLink to="/datasensor">Data Sensor</NavLink>
+				</li>
+				<li>
+					<NavLink to="/history">History</NavLink>
+				</li>
+				<li>
+					<NavLink to="/profile">Profile</NavLink>
+				</li>
+			</ul>
+		</header>
+	);
+};
