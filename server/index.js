eadd61be8acc3f6415ffb392a14bf34eda61979c
nqdat2002 <datfeed.2706@gmail.com>
2024-02-29 00:03:58 +0700

    init mqtt in node js

--- server/index.js
+++ server/index.js
@@ -1,13 +1,29 @@
 import express from "express";
-import todoRouter from './routers/todoRouter.js';
+import todoRouter from "./routers/todoRouter.js";
 
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("Hello world."));
+// app.get("/", (req, res) => res.send("Hello world."));
 
-app.use("/", todoRouter);
+// app.use("/", todoRouter);
 
+import mqtt from "mqtt";
+let client = mqtt.connect("mqtt://test.mosquitto.org");
+
+client.on("connect", () => {
+	client.subscribe("presence", (err) => {
+		if (!err) {
+			client.publish("presence", "Hello mqtt");
+		}
+	});
+});
+
+client.on("message", (topic, message) => {
+  	// message is Buffer
+	console.log(message.toString());
+	client.end();
+});
 const PORT = 5000;
 
 app.listen(PORT, () => console.log(`The app start on ${PORT}`));
