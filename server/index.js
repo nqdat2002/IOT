4ce646e23b5aacc932227328eac85bd7f8f0b79b
nqdat2002 <datfeed.2706@gmail.com>
2024-03-18 19:45:06 +0700

    update mqtt Client

--- server/index.js
+++ server/index.js
@@ -1,13 +1,10 @@
 import express from "express";
-import todoRouter from "./routers/todoRouter.js";
 import dataSensor from "./routers/dataSensor.js";
-import {CreateDataSensor} from "./controllers/dataSensor.js";
+import actionHistory from "./routers/actionHistory.js";
 
 import dotenv from "dotenv";
-// import bodyParser from "body-parser";
 import cors from "cors";
-import mqtt from "mqtt";
-import formatDate from "./utils/formatDate.js";
+import mqttClient from "./mqttClient.js";
 
 dotenv.config({ path: "./config.env" });
 const app = express();
@@ -27,68 +24,15 @@ app.get("/", (req, res) => res.send("Hello world."));
 
 // app.use("/", todoRouter);
 app.use("/api/datasensor", dataSensor);
+app.use("/api/actionhistory", actionHistory);
 
+const topics_sub = ["esp32/datasensors", "esp32/ledStatus", "esp32/temperature", "esp32/humidity", "esp32/luminosity"];
+const topics_pub = ["esp32/led/LED_02", "esp32/led/LED_04", "esp32/led/LED_All", "esp32/led"];
 
-
-const options = {
-	clientId: process.env.MQTT_CLIENT_ID,
-	username: process.env.MQTT_USERNAME,
-	password: process.env.MQTT_PASSWORD,
-	clean: true
-};
-
-const MQTT_IP_ADDRESS = process.env.MQTT_PROTOCOL + "://" + process.env.MQTT_HOST + ":" + process.env.MQTT_PORT;
-let client = mqtt.connect(`${MQTT_IP_ADDRESS}`, options);
-
-let topicValues = {
-    "temperature": null,
-	"humidity": null,
-    "luminosity": null,
-	"dateCreated": null,
-}
-
-
-const topics_sub = ["esp32/datasensors", "esp32/temperature", "esp32/humidity", "esp32/luminosity"];
-const topics_pub = ["esp32/led/LED_02", "esp32/led/LED_04", "esp32/led/LED_All", "esp32/led/LED_status"];
-
-// connect the client to the broker
-console.log("connected flag  " + client.connected);
-
-client.on("connect", () => {	
-	console.log("connected  " + client.connected);
-});
-
-client.on("connect", () => {
-	client.subscribe(topics_sub, (err) => {
-		if (!err) {
-			// client.publish("presence", "Hello mqtt");
-			console.log("Subscribed to topics: ", topics_sub);
-		}
-	});
-});
-client.on("message", (topic, message) => {
-	if (topic == "esp32/datasensors"){
-		try {
-			const now = formatDate(new Date());
-			const data = JSON.parse(message.toString());
-			topicValues["temperature"] = data.temperature;
-			topicValues["humidity"] = data.humidity;
-			topicValues["luminosity"] = data.luminosity;
-			topicValues["dateCreated"] = now;
-			console.log("Date Created: ", now);
-			console.log("Temperature: ", data.temperature);
-			console.log("Humidity: ", data.humidity);
-			console.log("Luminosity: ", data.luminosity);	
-
-			// save data to db
-			CreateDataSensor(topicValues);
-		} 
-		catch (error) {
-			console.error('Error parsing JSON:', error);
-		}
-	}
-});
-
+// mqtt client connect, publish and subscribe
+// mqttClient.connectMqtt();
+mqttClient.subMqtt(topics_sub);
+mqttClient.msgMqtt();
 
 const PORT = process.env.PORT || 8080;
 
---
+++ server/mqttClient.js
@@ -0,0 +1,127 @@
+import mqtt from "mqtt";
+import dotenv from "dotenv";
+dotenv.config({ path: "./config.env" });
+import formatDate from "./utils/formatDate.js";
+import {CreateDataSensor} from "./controllers/dataSensor.js";
+
+const options = {
+	clientId: process.env.MQTT_CLIENT_ID,
+	username: process.env.MQTT_USERNAME,
+	password: process.env.MQTT_PASSWORD,
+	clean: true
+};
+const MQTT_IP_ADDRESS = process.env.MQTT_PROTOCOL + "://" + process.env.MQTT_HOST + ":" + process.env.MQTT_PORT;
+
+let topicValues = {
+    "temperature": null,
+	"humidity": null,
+    "luminosity": null,
+	"dateCreated": null,
+};
+
+let ledStatus = {
+    "Led_1": "off",
+    "Led_2": "off",
+}
+
+const updateLedStatus = (led, status) => {
+    if (ledStatus[led] !== status) {
+        ledStatus[led] = status;
+        console.log("now LedStatus: ", ledStatus);
+    }
+};
+
+
+const mqttClient = {
+    client: null,
+    connectMqtt: () => {
+        mqttClient.client = mqtt.connect(`${MQTT_IP_ADDRESS}`, options);
+        console.log("connected flag  " + mqttClient.client.connected);
+        mqttClient.client.on("connect", () => {	
+	        console.log("connected with " + mqttClient.client.connected);
+        });
+    },
+
+    pubMqtt: (topic, message) => {
+        mqttClient.client.on("connect", () => {
+            // console.log("connected  " + client.connected);
+            mqttClient.client.publish(topic, message, (err) => {
+                if(err){
+                    console.log("Error: ", err);
+                    return;
+                }
+                console.log("Message is published");
+                // client.end();
+            });
+        });
+        mqttClient.client.on('error', (err) => {
+            console.error('Error occurred:', err);
+        });
+    },
+
+    subMqtt: (topic) => {
+        mqttClient.client.on("connect", () => {
+            mqttClient.client.subscribe(topic, (err) => {
+                if (!err) {
+                    console.log("Subscribed to topics: ", topic);
+                }
+            });
+        });
+
+        mqttClient.client.on('error', (err) => {
+            console.error('Error occurred:', err);
+        });
+    },
+
+    // message received from the broker
+    msgMqtt: () => {
+        mqttClient.client.on("message", (topic, message) => {
+            if (topic == "esp32/datasensors"){
+                try {
+                    const now = formatDate(new Date());
+                    const data = JSON.parse(message.toString());
+                    topicValues["temperature"] = data.temperature;
+                    topicValues["humidity"] = data.humidity;
+                    topicValues["luminosity"] = data.luminosity;
+                    topicValues["dateCreated"] = now;
+                    console.log("Date Created: ", now);
+                    console.log("Temperature: ", data.temperature);
+                    console.log("Humidity: ", data.humidity);
+                    console.log("Luminosity: ", data.luminosity);	
+        
+                    // save data to db
+                    CreateDataSensor(topicValues);
+                } 
+                catch (error) {
+                    console.error('Error parsing JSON:', error);
+                }
+            }
+
+            if (topic == "esp32/ledStatus"){
+                try {
+                    const now = formatDate(new Date());
+                    const data = JSON.parse(message.toString());
+                    console.log("Recieved Msg: ", data);
+                    console.log("prev LedStatus: ", ledStatus);
+
+                    for (const led in ledStatus) {
+                        if (ledStatus.hasOwnProperty(led)) {
+                            updateLedStatus(led, data[led].substring(3));
+                        }
+                    }
+                    // save data to db
+                    // CreateActionHistory(data);
+                } 
+                catch (error) {
+                    console.error('Error parsing JSON:', error);
+                }
+            }
+        });
+        mqttClient.client.on('error', (err) => {
+            console.error('Error occurred:', err);
+        });
+    }
+};
+
+export default mqttClient;
+mqttClient.connectMqtt();
